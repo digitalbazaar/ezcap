8d8719311b43048b96abb7b2866527d073c902a2
Manu Sporny <msporny@digitalbazaar.com>
2021-01-08 18:02:18 -0500

    Update test suite to new API.

--- tests/unit/EzcapClient.spec.js
+++ tests/unit/EzcapClient.spec.js
@@ -1,20 +1,29 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
+import {Ed25519VerificationKey2018}
+  from '@digitalbazaar/ed25519-verification-key-2018';
+import {ZcapClient} from '../../';
 import chai from 'chai';
+import didKey from 'did-method-key';
 import dirtyChai from 'dirty-chai';
+
 chai.use(dirtyChai);
 chai.should();
 const {expect} = chai;
+const didKeyDriver = didKey.driver();
 
-import {EzcapClient} from '../../';
-
-describe('EzcapClient', () => {
+describe('ZcapClient', () => {
   describe('constructor', () => {
-    it('should create an EzcapClient', async () => {
-      const ezcap = new EzcapClient();
+    it('should create an ZcapClient', async () => {
+      const baseUrl = 'https://zcap.example/';
+      const keypair = await Ed25519VerificationKey2018.generate();
+      const didDoc = await didKeyDriver.keyToDidDoc(keypair);
+      const invocationSigner = keypair.signer();
+      invocationSigner.id = didDoc.capabilityInvocation[0];
+      const zcapClient = new ZcapClient({baseUrl, invocationSigner});
 
-      expect(ezcap).to.exist();
+      expect(zcapClient).to.exist();
     });
   });
 });
