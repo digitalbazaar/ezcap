62ceac52b9adaa800573a293fb547a5762e64766
Manu Sporny <msporny@digitalbazaar.com>
2021-01-19 16:12:54 -0500

    Implement capability delegation.

--- tests/unit/EzcapClient.spec.js
+++ tests/unit/EzcapClient.spec.js
@@ -1,8 +1,6 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-import {Ed25519VerificationKey2018}
-  from '@digitalbazaar/ed25519-verification-key-2018';
 import {ZcapClient} from '../../';
 import chai from 'chai';
 import didKey from 'did-method-key';
@@ -16,14 +14,40 @@ const didKeyDriver = didKey.driver();
 describe('ZcapClient', () => {
   describe('constructor', () => {
     it('should create an ZcapClient', async () => {
-      const baseUrl = 'https://zcap.example/';
-      const keypair = await Ed25519VerificationKey2018.generate();
-      const didDoc = await didKeyDriver.keyToDidDoc(keypair);
-      const invocationSigner = keypair.signer();
-      invocationSigner.id = didDoc.capabilityInvocation[0];
-      const zcapClient = new ZcapClient({baseUrl, invocationSigner});
+      const baseUrl = 'https://zcap.example';
+      const didDocument = await didKeyDriver.generate();
+      const delegationKeypair =
+        didDocument.keys[didDocument.capabilityDelegation[0]];
+      const invocationKeypair =
+        didDocument.keys[didDocument.capabilityInvocation[0]];
+      const zcapClient = new ZcapClient({
+        baseUrl, delegationKeypair, invocationKeypair
+      });
 
       expect(zcapClient).to.exist();
+    });
+    it('should delegate a zcap', async () => {
+      const baseUrl = 'https://zcap.example';
+      const didDocument = await didKeyDriver.generate();
+      const delegationKeypair =
+        didDocument.keys[didDocument.capabilityDelegation[0]];
+      const invocationKeypair =
+        didDocument.keys[didDocument.capabilityInvocation[0]];
+      const zcapClient = new ZcapClient({
+        baseUrl, delegationKeypair, invocationKeypair
+      });
+      expect(zcapClient).to.exist();
+
+      const url = baseUrl + '/items';
+      const delegate =
+        'did:key:z6MkogR2ZPr4ZGvLV2wZ7cWUamNMhpg3bkVeXARDBrKQVn2c';
+      const delegatedZcap = await zcapClient.delegate({url, delegate});
+
+      delegatedZcap.parentCapability.should.equal('urn:zcap:' + url);
+      delegatedZcap.controller.should.equal(delegate);
+      delegatedZcap.proof.proofPurpose.should.equal('capabilityDelegation');
+      delegatedZcap.proof.capabilityChain.should.have.length(2);
+      //const delegatedZcap = zcapClient.delegate({capability, delegate});
     });
   });
 });
