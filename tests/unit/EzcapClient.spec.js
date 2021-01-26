9612f0cee6d2886782424d7fc1a6845036d5d926
Manu Sporny <msporny@digitalbazaar.com>
2021-01-26 16:47:42 -0500

    Rename invoke->request. Allow didDocument, keyPairs in constructor.

--- tests/unit/EzcapClient.spec.js
+++ tests/unit/EzcapClient.spec.js
@@ -13,7 +13,14 @@ const didKeyDriver = didKey.driver();
 
 describe('ZcapClient', () => {
   describe('constructor', () => {
-    it('should create an ZcapClient', async () => {
+    it('should create an ZcapClient using a didDocument', async () => {
+      const baseUrl = 'https://zcap.example';
+      const {didDocument, keyPairs} = await didKeyDriver.generate();
+      const zcapClient = new ZcapClient({baseUrl, didDocument, keyPairs});
+
+      expect(zcapClient).to.exist();
+    });
+    it('should create an ZcapClient using signers', async () => {
       const baseUrl = 'https://zcap.example';
       const {didDocument, keyPairs} = await didKeyDriver.generate();
       const {invocationSigner, delegationSigner} = getCapabilitySigners({
@@ -35,12 +42,12 @@ describe('ZcapClient', () => {
       expect(zcapClient).to.exist();
 
       const url = baseUrl + '/items';
-      const delegate =
+      const targetDelegate =
         'did:key:z6MkogR2ZPr4ZGvLV2wZ7cWUamNMhpg3bkVeXARDBrKQVn2c';
-      const delegatedZcap = await zcapClient.delegate({url, delegate});
+      const delegatedZcap = await zcapClient.delegate({url, targetDelegate});
 
       delegatedZcap.parentCapability.should.equal('urn:zcap:' + url);
-      delegatedZcap.controller.should.equal(delegate);
+      delegatedZcap.controller.should.equal(targetDelegate);
       delegatedZcap.proof.proofPurpose.should.equal('capabilityDelegation');
       delegatedZcap.proof.capabilityChain.should.have.length(1);
     });
