ccbe9d18accb88170da65fd5312589416389d715
James Easter <jeaster@digitalbazaar.com>
2024-06-27 15:50:05 -0600

    Add test to delegate root zcap with ed25519-multikey.

--- tests/unit/EzcapClient.spec.js
+++ tests/unit/EzcapClient.spec.js
@@ -2,6 +2,7 @@
  * Copyright (c) 2020-2023 Digital Bazaar, Inc. All rights reserved.
  */
 import * as didKey from '@digitalbazaar/did-method-key';
+import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
 import chai from 'chai';
 import {Ed25519Signature2020} from '@digitalbazaar/ed25519-signature-2020';
 import {Ed25519VerificationKey2020} from
@@ -55,6 +56,36 @@ describe('ZcapClient', () => {
         await didKeyDriver.fromKeyPair({verificationKeyPair});
       // this `id` value manifest as the `verificationMethod` on the proof
       // see the assertion below
+      verificationKeyPair.id = didDocument.verificationMethod[0].id;
+      const zcapClient = new ZcapClient({
+        SuiteClass: Ed25519Signature2020,
+        invocationSigner: verificationKeyPair.signer(),
+        delegationSigner: verificationKeyPair.signer(),
+      });
+      expect(zcapClient).to.exist;
+      const url = 'https://zcap.example/items';
+      const controller =
+      'did:key:z6MkogR2ZPr4ZGvLV2wZ7cWUamNMhpg3bkVeXARDBrKQVn2c';
+      const delegatedZcap = await zcapClient.delegate({
+        invocationTarget: url, controller
+      });
+      delegatedZcap.parentCapability.should.equal(
+        'urn:zcap:root:' + encodeURIComponent(url));
+      delegatedZcap.controller.should.equal(controller);
+      delegatedZcap.proof.proofPurpose.should.equal('capabilityDelegation');
+      delegatedZcap.proof.capabilityChain.should.have.length(1);
+      delegatedZcap.proof.verificationMethod.should.equal(
+        verificationKeyPair.id);
+    });
+    it('should delegate a root zcap using Ed25519Multikey', async () => {
+      const verificationKeyPair = await Ed25519Multikey.generate();
+      didKeyDriver.use({
+        multibaseMultikeyHeader: 'z6Mk',
+        fromMultibase: Ed25519Multikey.from
+      });
+      const {didDocument} =
+        await didKeyDriver.fromKeyPair({verificationKeyPair});
+      // this `id` value manifest as the `verificationMethod` on the proof
       verificationKeyPair.id = didDocument.verificationMethod[0].id;
       const zcapClient = new ZcapClient({
         SuiteClass: Ed25519Signature2020,
