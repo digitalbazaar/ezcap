816aa0c0b857f5194674a667a87ed665c3809cfe
David I. Lehn <dlehn@digitalbazaar.com>
2026-03-06 16:51:19 -0500

    Update eslint.
    - Use `eslint@9`.
    - Use `@digitalbazaar/eslint-config`.

--- tests/karma.conf.cjs
+++ tests/karma.conf.cjs
@@ -37,7 +37,7 @@ module.exports = function(config) {
     // available preprocessors:
     // https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'unit/*.js': preprocessors,
+      'unit/*.js': preprocessors
     },
 
     webpack: {
